5c70fa3f20cb4ef78f2d888da710cb1c9d20e5fb
naushad <nkmission786@gmail.com>
2023-06-10 19:06:42 +0530

    done

--- src/main/java/com/driver/services/AdminService.java
+++ src/main/java/com/driver/services/AdminService.java
@@ -1,20 +1,64 @@
-package com.driver.services;
+package com.driver.services.impl;
 
 import java.util.List;
+
+import com.driver.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.driver.model.Admin;
 import com.driver.model.Customer;
 import com.driver.model.Driver;
+import com.driver.repository.AdminRepository;
+import com.driver.repository.CustomerRepository;
+import com.driver.repository.DriverRepository;
 
-public interface AdminService {
+@Service
+public class AdminServiceImpl implements AdminService {
 
-	public void adminRegister(Admin admin);
+	@Autowired
+	AdminRepository adminRepository1;
 
-	public Admin updatePassword(Integer adminId, String password);
+	@Autowired
+	DriverRepository driverRepository1;
 
-	public void deleteAdmin(int adminId);
+	@Autowired
+	CustomerRepository customerRepository1;
 
-	public List<Driver> getListOfDrivers();
-	
-	public List<Customer> getListOfCustomers();
+	@Override
+	public void adminRegister(Admin admin) {
+		//Save the admin in the database
+		adminRepository1.save(admin);
+	}
+
+	@Override
+	public Admin updatePassword(Integer adminId, String password) {
+		//Update the password of admin with given id
+		Admin admin = adminRepository1.findById(adminId).get();
+		admin.setPassword(password);
+		adminRepository1.save(admin);
+
+		return admin;
+
+	}
+
+	@Override
+	public void deleteAdmin(int adminId){
+		// Delete admin without using deleteById function
+		Admin admin = adminRepository1.findById(adminId).get();
+		adminRepository1.delete(admin);
+	}
+
+	@Override
+	public List<Driver> getListOfDrivers() {
+		//Find the list of all drivers
+		return driverRepository1.findAll();
+	}
+
+	@Override
+	public List<Customer> getListOfCustomers() {
+		//Find the list of all customers
+		return customerRepository1.findAll();
+	}
+
 }
